39fdddf6669f9b3513593723becebf953676cf3b
KyselovaMaria <mariyka.mk@gmail.com>
2025-02-16 16:41:55 +0200

    1. UML та його застосування до зображень діаграм класів

---
+++ Homework1/Task1/Task3_1.java
@@ -0,0 +1,9 @@
+package Homework1.Task1;
+
+public class Task3_1 {
+    public static void main(String[] args) {
+        CarSimulator carSimulator = new CarSimulator();
+        carSimulator.buildCar(450, 2.5f, 2, 18, "Audi Q7");
+        System.out.println(carSimulator);
+    }
+}
